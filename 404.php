9fdefefbfff2151e1ccf9f903b234314ab637d5b
0pp0s1t3 <RICHARDOLECOCO@gmail.com>
2025-04-01 11:53:50 -0400

    commit2

--- 404.php
+++ 404.php
@@ -1,18 +1,5 @@
 
 <?php get_header() ?>
 
-    <section class="erreur">
-        <div class="boiteflex erreur"> 
-        <h1 class="errorTitle">Oops, you've landed on Island 404!</h1>
-        <p class="errorPara">Don't worry, explorer! You seem to have drifted away from your dream destinations. Navigate back to find your perfect adventure.</p>
-        <a href="index.php" class="btn">Return to Home</a>
-        <div class="suggestions">
-            <a href="#">Costa Rica</a>
-            <a href="#">Alaska</a>
-            <a href="#">Faroe Islands</a>
-            <a href="#">Paris, France</a>     
-        </div>
-    </section>
+    
     <?php get_footer(); ?>
-</body>
-</html>
