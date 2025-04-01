276471405ae9864280a6e41a16bf4a8fae6836c6
0pp0s1t3 <RICHARDOLECOCO@gmail.com>
2025-04-01 11:44:32 -0400

    commit1

--- 404.php
+++ 404.php
@@ -1,15 +1,10 @@
-<?php
-/**
- *  index.php est le modèle par défaut
- *  si aucun modèle peut satisfaire la requête http dans ce cas c'est index.php qui affichera le contenu de la page
- */
-?>
+
 <?php get_header() ?>
 
     <section class="erreur">
         <div class="boiteflex erreur"> 
-        <h1>Oops, you've landed on Island 404!</h1>
-        <p>Don't worry, explorer! You seem to have drifted away from your dream destinations. Navigate back to find your perfect adventure.</p>
+        <h1 class="errorTitle">Oops, you've landed on Island 404!</h1>
+        <p class="errorPara">Don't worry, explorer! You seem to have drifted away from your dream destinations. Navigate back to find your perfect adventure.</p>
         <a href="index.php" class="btn">Return to Home</a>
         <div class="suggestions">
             <a href="#">Costa Rica</a>
